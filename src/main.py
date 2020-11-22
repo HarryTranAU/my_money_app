67d320a119e64bd04ebe8ec5554335828126c119
Harry Tran <css012013@coderacademy.edu.au>
2020-11-22 14:45:19 +1100

    flake8 fixes

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/register", methods=["POST"])
 def user_create():
     return "POST function"
 
+
 @app.route("/<int:id>", methods=["GET"])
 def user_profile(id):
     return "GET function"
 
+
 @app.route("/<int:id>", methods=["PUT"])
 def user_update(id):
     return "PUT function"
+
 
 @app.route("/<int:id>", methods=["DELETE"])
 def user_delete(id):
