b9ca3120e7bdb9bbab8bdf6b7105f7f9d609412a
Harry Tran <css012013@coderacademy.edu.au>
2020-11-23 16:03:49 +1100

    remove database.py, change to factory pattern

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 from dotenv import load_dotenv
-from flask import Flask, jsonify
-from database import init_db
-
 load_dotenv()
-app = Flask(__name__)
-app.config.from_object("settings.app_config")
-db = init_db(app)
 
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-ma = Marshmallow(app)
 
-from controllers import registerable_controllers
-for controller in registerable_controllers:
-    app.register_blueprint(controller)
+db = SQLAlchemy()
+ma = Marshmallow()
 
-from marshmallow.exceptions import ValidationError
 
-@app.errorhandler(ValidationError)
-def handle_bad_request(error):
-    return(jsonify(error.messages), 400)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("settings.app_config")
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    from controllers import registerable_controllers
+
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+
+    from marshmallow.exceptions import ValidationError
+
+    @app.errorhandler(ValidationError)
+    def handle_bad_request(error):
+        return(jsonify(error.messages), 400)
+
+    return app
