0520eebb5adb2ef5b3b038103028d9526d91ef87
Harry Tran <css012013@coderacademy.edu.au>
2020-11-23 15:09:04 +1100

    added db-custom seed

--- src/commands.py
+++ src/commands.py
@@ -14,3 +14,19 @@ def create_db():
 def drop_db():
     db.drop_all()
     print("Tables Deleted")
+
+
+@db_commands.cli.command("seed")
+def seed_db():
+    from models.Budget import Budget
+    from faker import Faker
+    faker = Faker()
+
+    for i in range(10):
+        budget = Budget()
+        budget.name = faker.catch_phrase()
+        db.session.add(budget)
+        print(f"{i+1} budget record(s) created")
+
+    db.session.commit()
+    print("Tables seeded")
