de92ab6e0a53b818290c0e14ef7f8511e08f9df3
Harry Tran <css012013@coderacademy.edu.au>
2020-11-24 17:40:24 +1100

    added jwt_required

--- src/controllers/budget_controller.py
+++ src/controllers/budget_controller.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, jsonify, request, abort
+from flask_jwt_extended import jwt_required
 from models.Budget import Budget
+from models.User import User
 from schemas.BudgetSchema import budgets_schema, budget_schema
 from main import db
 budgets = Blueprint("budgets", __name__, url_prefix="/budget")
 
 
 @budgets.route("/", methods=["POST"])
+@jwt_required
 def budget_create():
     budget_fields = budget_schema.load(request.json)
     new_budget = Budget()
@@ -17,18 +20,21 @@ def budget_create():
 
 
 @budgets.route("/", methods=["GET"])
+@jwt_required
 def budget_index():
     budgets = Budget.query.all()
     return jsonify(budgets_schema.dump(budgets))
 
 
 @budgets.route("/<int:id>", methods=["GET"])
+@jwt_required
 def budget_show(id):
     budget = Budget.query.get(id)
     return jsonify(budget_schema.dump(budget))
 
 
 @budgets.route("/<int:id>", methods=["PUT", "PATCH"])
+@jwt_required
 def budget_update(id):
     budgets = Budget.query.filter_by(id=id)
     budget_fields = budget_schema.load(request.json)
@@ -38,6 +44,7 @@ def budget_update(id):
 
 
 @budgets.route("/<int:id>", methods=["DELETE"])
+@jwt_required
 def budget_delete(id):
     budget = Budget.query.get(id)
 
